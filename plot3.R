10454cfffbd491100d2fdf8630917fa9735e521a
farchide <operations@mahdavipour.com>
2014-05-11 13:22:52 +0430

    plot 3 first rev

---
+++ plot3.R
@@ -0,0 +1,18 @@
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+td = tempdir()
+tf = tempfile(tmpdir=td, fileext=".zip")
+download.file(url, tf)
+fname = unzip(tf, list=TRUE)$Name[1]
+unzip(tf, files=fname, exdir=td, overwrite=TRUE)
+fpath = file.path(td, fname)
+plotdata <- read.table(fpath, header = TRUE,sep=";",na.strings="?")
+plotsubset <- subset(plotdata, plotdata$Date >= as.Date("2007-2-1","%Y-%m-%d") & plotdata$Date <= as.Date("2007-2-2","%Y-%m-%d"))
+install.packages("chron")
+library(chron)
+dateandtime <- chron(as.character(plotsubset$Date),plotsubset$Time,format = c(dates = "Y-m-d", times = "h:m:s"))
+plot(dateandtime,plotsubset$Sub_metering_1,type="l",ylab="Energy sub metering", xlab="")
+lines(dateandtime,plotsubset$Sub_metering_2,col="red")
+lines(dateandtime,plotsubset$Sub_metering_3,col="blue")
+legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lwd = 0.5, cex = 0.8,lty = c(1, 1, 1), pch =c(NA,NA,NA))
+dev.copy(png, file = "plot3.png")
+dev.off()
