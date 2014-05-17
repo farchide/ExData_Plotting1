15a7f8c084a83cbd73dfcae6deae8507a72ca0ca
Farshid Mahdavipour <operations@mahdavipour.com>
2014-05-18 00:44:51 +0430

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,13 +1,13 @@
-#url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-#td = tempdir()
-#tf = tempfile(tmpdir=td, fileext=".zip")
-#download.file(url, tf)
-#fname = unzip(tf, list=TRUE)$Name[1]
-#unzip(tf, files=fname, exdir=td, overwrite=TRUE)
-#fpath = file.path(td, fname)
-#plotdata <- read.table(fpath, header = TRUE,sep=";",na.strings="?")
-#plotsubset <- subset(plotdata, plotdata$Date >= as.Date("2007-2-1","%Y-%m-%d") & plotdata$Date <= as.Date("2007-2-2","%Y-%m-%d"))
-#install.packages("chron")
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+td = tempdir()
+tf = tempfile(tmpdir=td, fileext=".zip")
+download.file(url, tf)
+fname = unzip(tf, list=TRUE)$Name[1]
+unzip(tf, files=fname, exdir=td, overwrite=TRUE)
+fpath = file.path(td, fname)
+plotdata <- read.table(fpath, header = TRUE,sep=";",na.strings="?")
+plotsubset <- subset(plotdata, plotdata$Date >= as.Date("2007-2-1","%Y-%m-%d") & plotdata$Date <= as.Date("2007-2-2","%Y-%m-%d"))
+install.packages("chron")
 library(chron)
 dateandtime <- chron(as.character(plotsubset$Date),plotsubset$Time,format = c(dates = "Y-m-d", times = "h:m:s"))
 par(mfrow=c(2,2))
